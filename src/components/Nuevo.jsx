0706ceee0c9958023f1e3985ef4acd3430ff484d
pipe473 <bedoyafelipe743@gmail.com>
2021-07-08 18:25:02 +0200

    Adding a form to create products

--- src/components/Nuevo.jsx
+++ src/components/Nuevo.jsx
@@ -2,7 +2,42 @@ import React from 'react';
 
 const Nuevo = () => {
     return ( 
-        <h1>Desde  nuevo productos jsx</h1>
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card">
+                    <div className="card-body">
+                        <h3 className="text-center mb-4 font-weight-bold">
+                            Agrega un nuevo Producto
+                        </h3>
+                        <form>
+                            <div className="form-group">
+                                <label>Nombre Producto</label>
+                                <input 
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Escribe tu nombre"
+                                    name="producto"
+                                    />
+                            </div>
+                            <div className="form-group">
+                                <label>Precio Producto</label>
+                                <input 
+                                    type="number"
+                                    className="form-control"
+                                    placeholder="Escribe tu nombre"
+                                    name="precio"
+                                    />
+                            </div>
+                            <button
+                                type="submit"
+                                className="btn btn-secondary font-weight-bold text-uppercase
+                                d-block w-100"
+                                >Agregar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
      );
 }
  

6f0664708f3a02384928d545e45360ca55aefba9
pipe473 <bedoyafelipe743@gmail.com>
2021-07-09 18:43:58 +0200

    Adding a new product to the state

--- src/actions/productoActions.js
+++ src/actions/productoActions.js
@@ -6,7 +6,30 @@ import {
 
 // CREAR NUEVOS PRODUCTOS
 export function createNewProductAction(product){
-    return () => {
-        console.log(product);        
+    return (dispatch) => {
+           dispatch(agregarProducto());
+
+           try {
+               dispatch( agregarProduct_OK(product) )
+           } catch (error) {
+               dispatch( agregarProducto_KO(true) )
+           }
     }
 }
+
+const agregarProducto = () =>({
+    type: AGREGAR_PRODUCTO,
+    payload: true
+});
+
+// SI EL PRODUCTO SE GUARDA EN LA BASE DE DATOS
+const agregarProduct_OK = product =>({
+    type: AGREGAR_PRODUCTO_EXITO,
+    payload: product
+});
+
+// SI HUBO UN ERROR
+
+const agregarProducto_KO = () =>({
+
+})
